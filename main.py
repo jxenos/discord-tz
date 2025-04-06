b46eccf26b25115089b9eec98a3593b844d2435a
Jorj Xenos <JorjXenos@gmail.com>
2025-04-06 18:07:29 +0000

    use webhook instead

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-import discord
 import requests
 from bs4 import BeautifulSoup
 import os
 import logging
-import asyncio
 
-# Discord client setup
-intents = discord.Intents.default()
-intents.messages = True
-client = discord.Client(intents=intents)
-last_notified_zone = None
+WEBHOOK_URL = os.getenv('WEBHOOK_URL')
 
 TARGET_ZONES = [
     'Catacombs',
@@ -27,13 +21,7 @@ TARGET_ZONES = [
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
-
-async def check_terror_zone():
-    global last_notified_zone
-    client_channel = client.get_channel(CHANNEL_ID)
-
+def check_terror_zone():
     try:
         response = requests.get('https://www.d2emu.com/tz')
         response.raise_for_status()
@@ -44,21 +32,14 @@ async def check_terror_zone():
             next_zone_header = tz_tracker.find('h2', string='Next Terror Zone:')
             if next_zone_header:
                 next_zone = next_zone_header.find_next_sibling('p').get_text(strip=True)
-
-                if any(zone.lower() in next_zone.lower() for zone in TARGET_ZONES) and next_zone != last_notified_zone:
-                    await client_channel.send(f'⚔️ **Upcoming Terror Zone:** {next_zone}!')
+                if any(zone.lower() in next_zone.lower() for zone in TARGET_ZONES):
+                    message = {"content": f'⚔️ **Upcoming Terror Zone:** {next_zone}!'}
+                    requests.post(WEBHOOK_URL, json=message)
                     logger.info(f"Pinged zone: {next_zone}")
-                    last_notified_zone = next_zone
                 else:
-                    logger.info(f"No match found for: {next_zone}")
-
+                    logger.info(f"No target zone match found for: {next_zone}")
     except Exception as e:
         logger.error(f"Error fetching or parsing the webpage: {e}")
 
-@client.event
-async def on_ready():
-    logger.info(f'Logged in as {client.user.name}')
-    await check_terror_zone()
-    await client.close()
-
-client.run(DISCORD_TOKEN)
+if __name__ == "__main__":
+    check_terror_zone()
